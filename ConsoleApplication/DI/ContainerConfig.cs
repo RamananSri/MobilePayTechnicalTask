c7d2104d22307ef1e07993b87f7025a17c15d7e7
RamananSri <18514299+RamananSri@users.noreply.github.com>
2020-06-01 00:26:49 +0200

    DI container with Autofac

---
+++ ConsoleApplication/DI/ContainerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace ConsoleApplication.DI
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Container { get; private set; }
+
+        public static void Register()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            //Registrations
+
+            Container = containerBuilder.Build();
+        }
+    }
+}

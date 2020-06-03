e5fbaa64573c6baa144163bfa9be855358f20f57
RamananSri <18514299+RamananSri@users.noreply.github.com>
2020-06-03 09:22:24 +0200

    Added unit tests

---
+++ LogComponent.UnitTests/AsyncFileLoggerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using LogComponent.Models;
+using LogTest;
+using Moq;
+using NUnit.Framework;
+
+namespace LogComponent.UnitTests
+{
+    [TestFixture]
+    public class AsyncFileLoggerTests : LoggerTests<AsyncFileLogger>
+    {
+        private readonly Mock<IDateTimeProvider> _datetimeProviderMock = new Mock<IDateTimeProvider>();
+
+        protected override AsyncFileLogger CreateLogger()
+        {
+            var loggerConfig = new LoggerConfiguration
+            {
+                Filepath = DirectoryInformation.FullName,
+            };
+
+            return new AsyncFileLogger(loggerConfig, _datetimeProviderMock.Object);
+        }
+
+        private static IEnumerable DateValuesData()
+        {
+            yield return new TestCaseData(
+                new DateTime(2020, 01, 01,23,00,00), 
+                new DateTime(2020,01,02,01,00,00)).Returns(2);
+            yield return new TestCaseData(
+                new DateTime(2020, 12, 12,01,01,01),
+                new DateTime(2020, 12,12,01,02,00)).Returns(1);
+        }
+
+        [TestCaseSource(typeof(AsyncFileLoggerTests), nameof(DateValuesData))]
+        public override int WriteToLog_PassingMidnight_ReturnsExpectedAmountOfLogFiles(DateTime first, DateTime second)
+        {
+            _datetimeProviderMock.SetupSequence(e => e.Now)
+                .Returns(first)
+                .Returns(second);
+
+            var logger = CreateLogger();
+
+            logger.WriteToLog("HelloWorld");
+            logger.WriteToLog("HelloWorld");
+
+            return DirectoryInformation.GetFiles().Length;
+        }
+    }
+}
---
+++ LogComponent.UnitTests/LoggerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LogTest;
+using NUnit.Framework;
+
+namespace LogComponent.UnitTests
+{
+    [TestFixture]
+    public abstract class LoggerTests<TLogger> where TLogger : ILogger
+    {
+        protected abstract TLogger CreateLogger();
+
+        protected DirectoryInfo DirectoryInformation;
+
+        [SetUp]
+        public void Setup()
+        {
+            DirectoryInformation = Directory.CreateDirectory("C:\\Users\\ram\\Desktop\\Test2");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DirectoryInformation.Delete(true);
+        }
+
+        [TestCase("HelloWorld")]
+        public virtual void WriteToLog_WriteMessageToFile_LogContainsMessage(string message)
+        {
+            TLogger logger = CreateLogger();
+
+            logger.WriteToLog(message);
+            
+            var log = ReadLogLines().ToList();
+            Assert.That(log.Any(e => e.Contains(message)));
+        }
+
+        [Test]
+        public abstract int WriteToLog_PassingMidnight_ReturnsExpectedAmountOfLogFiles(DateTime first, DateTime second);
+
+        [Test]
+        public virtual void StopWithFlush_OutstandingLogEntries_Returns()
+        {
+            var iterations = 500;
+            var logger = CreateLogger();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                logger.WriteToLog($"Writing {i}");
+            }
+
+            logger.StopWithFlush();
+
+            var log = ReadLogLines().ToList().Count;
+
+            Assert.That(log == iterations+1);
+        }
+
+        [Test]
+        public virtual void StopWithoutFlush_WithOutstandingLogentries_Returns()
+        {
+            var iterations = 500;
+            var logger = CreateLogger();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                logger.WriteToLog($"Writing {i}");
+            }
+
+            logger.StopWithoutFlush();
+
+            var log = ReadLogLines();
+
+            Assert.That(log.Count() != iterations+1);
+        }
+
+        protected IEnumerable<string> ReadLogLines()
+        {
+            var logFilename = DirectoryInformation.GetFiles().Single().FullName;
+            return File.ReadLines(logFilename);
+        }
+    }
+}

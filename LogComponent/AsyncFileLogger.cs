e57a19714928a150c9e23f7a035cf59abdd3b7d0
RamananSri <ramanansdev@gmail.com>
2020-06-03 21:02:02 +0200

    Producer/Consumer pattern with BlockingCollection
    Using BlockingCollection instead of ConcurrentQueue directly

--- LogComponent/AsyncFileLogger.cs
+++ LogComponent/AsyncFileLogger.cs
@@ -11,110 +11,113 @@ namespace LogTest
 {
     public class AsyncFileLogger : ILogger
     {
+        private BlockingCollection<FileLogLine> _loqQueue;
+        private DateTime _latestLogTimestamp;
+
         private readonly IDateTimeProvider _dateTimeProvider;
         private readonly string _logPath;
-        private readonly ConcurrentQueue<FileLogLine> _lines; 
-        
-        private bool _exit;
-        private bool _quitWithFlush;
+        private readonly CancellationTokenSource cancellationTokenSource;
 
         public AsyncFileLogger(LoggerConfiguration loggerConfiguration, IDateTimeProvider dateTimeProvider)
         {
             _dateTimeProvider = dateTimeProvider ?? throw new ArgumentNullException(nameof(dateTimeProvider));
             _logPath = loggerConfiguration.Filepath ?? throw new ArgumentNullException(nameof(loggerConfiguration.Filepath));
 
-            _lines = new ConcurrentQueue<FileLogLine>();
+            _loqQueue = new BlockingCollection<FileLogLine>();
+            cancellationTokenSource = new CancellationTokenSource();
 
-            Task.Run(MainLoop);
+            Task.Run(() => StartLogging(cancellationTokenSource.Token));
         }
 
         public void StopWithoutFlush()
         {
-            _exit = true;
+            cancellationTokenSource.Cancel();
         }
 
         public void StopWithFlush()
         {
-            _quitWithFlush = true;
+            _loqQueue.CompleteAdding();
         }
 
         public void WriteToLog(string s)
         {
-            Console.WriteLine(new FileLogLine(s, _dateTimeProvider.Now).GetLineText());
-            _lines.Enqueue(new FileLogLine(s, _dateTimeProvider.Now));
+            var newLine = new FileLogLine(s, _dateTimeProvider.Now);
+            _loqQueue.Add(newLine);
         }
 
-        private void MainLoop()
+        private void StartLogging(CancellationToken cancellationToken)
         {
             Directory.CreateDirectory(_logPath);
 
-            var latestLogFileTimestamp = _dateTimeProvider.Now;
-            var isNewLog = true;
+            _latestLogTimestamp = _dateTimeProvider.Now;
 
-            while (!_exit)
+            while (true)
             {
-                if (_lines.Count > 0)
+                if (cancellationToken.IsCancellationRequested)
                 {
-                    using (StreamWriter streamWriter = new StreamWriter(GetLogFilepath(latestLogFileTimestamp),true, Encoding.UTF8, 65536))
-                    {
-                        if (isNewLog)
-                        {
-                            streamWriter.WriteLine(FileLogLine.GetFormattedFileHeader());
-                            isNewLog = false;
-                        } 
-
-                        foreach (FileLogLine logLine in _lines)
-                        {
-                            if (!_exit || _quitWithFlush)
-                            {
-
-
-                                //if ((DateTime.Now - _curDate).Days != 0)
-                                //{
-                                //    _curDate = DateTime.Now;
-
-                                //    _writer = File.AppendText(@"C:\LogTest\Log" + DateTime.Now.ToString("yyyyMMdd HHmmss fff") + ".log");
-
-                                //    _writer.Write("Timestamp".PadRight(25, ' ') + "\t" + "Data".PadRight(15, ' ') + "\t" + Environment.NewLine);
-
-                                //    stringBuilder.Append(Environment.NewLine);
-
-                                //    _writer.Write(stringBuilder.ToString());
-
-                                //    _writer.AutoFlush = true;
-                                //}
-
-                                streamWriter.WriteLine(logLine.GetLineText());
-
-                                if (!_lines.TryDequeue(out _))
-                                {
-                                    throw new ArgumentException("Failed to dequeue queue");
-                                }
-                            }
-                        }
-                    }
-
-                    if (_quitWithFlush && _lines.Count == 0)
-                    {
-                        _exit = true;
-                    }
-                        
-                    Thread.Sleep(50);
+                    return;
                 }
+
+                try
+                {
+                    FileLogLine line = _loqQueue.Take();
+                    Log(line);
+
+                }
+                catch (TaskCanceledException ex)
+                {
+                    break;
+                }
+                catch (Exception e) //Reset the log queue to keep going
+                {
+                    _loqQueue = new BlockingCollection<FileLogLine>();
+                    continue;
+                }
+                finally
+                {
+                    cancellationTokenSource.Dispose();
+                }
+            }
+
+            Thread.Sleep(50);
+        }
+
+        private void Log(FileLogLine line)
+        {
+            if (IsNewDay(_latestLogTimestamp))
+            {
+                _latestLogTimestamp = _dateTimeProvider.Now;
+                WriteLogLine(line, true);
+            }
+            else
+            {
+                WriteLogLine(line);
             }
         }
 
-        //private void CreateNewLog()
-        //{
-        //    _writer = File.AppendText($@"{_logPath}\Log{_dateTimeProvider.Now:yyyyMMdd HHmmss fff}.log");
-        //    _writer.WriteLineAsync(FileLogLine.GetFormattedFileHeader());
-        //    _writer.AutoFlush = true;
-        //}
+        private void WriteLogLine(FileLogLine line, bool includeHeader = false)
+        {
+            var filepath = GetLogFilepath(_latestLogTimestamp);
+
+            using (StreamWriter streamWriter = new StreamWriter(filepath, true, Encoding.UTF8, 65536))
+            {
+                if (includeHeader)
+                {
+                    streamWriter.WriteLine(FileLogLine.GetFormattedFileHeader());
+                }
+
+                streamWriter.WriteLine(line.GetLineText());
+            }
+        }
 
         private string GetLogFilepath(DateTime timestamp)
         {
             return Path.Combine(_logPath, $@"Log{timestamp:yyyyMMdd HHmmss fff}.log");
         }
 
+        private bool IsNewDay(DateTime latestLog)
+        {
+            return latestLog.Date == _dateTimeProvider.Now.Date;
+        }
     }
 }

bfe2e18a55ade31d8a2bb86a99b43a7dc53a449b
RamananSri <18514299+RamananSri@users.noreply.github.com>
2020-06-03 09:21:56 +0200

    Added FileLogLine with file formatting

---
+++ LogComponent/Models/FileLogLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace LogComponent.Models
+{
+    public class FileLogLine : LogLine
+    {
+        public FileLogLine(string text, DateTime timestamp) : base(text, timestamp)
+        { }
+
+        public static string GetFormattedFileHeader()
+        {
+            var stringbuilder = new StringBuilder();
+            stringbuilder.Append("Timestamp".PadRight(25, ' '));
+            stringbuilder.Append("\t");
+            stringbuilder.Append("Data".PadRight(15, ' '));
+            stringbuilder.Append("\t");
+            stringbuilder.Append("\t");
+
+            return stringbuilder.ToString();
+        }
+
+        public override string GetLineText()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(Timestamp.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+            stringBuilder.Append("\t");
+            stringBuilder.Append(base.GetLineText());
+            stringBuilder.Append("\t");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ LogComponent/Models/LogLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace LogComponent.Models
+{
+    public class LogLine
+    {
+        public LogLine(string text, DateTime timestamp)
+        {
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+            Timestamp = timestamp;
+        }
+
+        public string Text { get; }
+        public DateTime Timestamp { get; }
+
+        public virtual string GetLineText()
+        {
+            var sb = new StringBuilder();
+
+            if (Text.Length > 0)
+            {
+                sb.Append(Text);
+                sb.Append(". ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
